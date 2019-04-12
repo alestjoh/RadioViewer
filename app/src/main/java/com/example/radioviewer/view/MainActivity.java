7aa2167a4a0e4ae7f0530f099d2dfec586dffb5c
alestjoh <ZUBEqevy42>
2019-04-12 14:57:22 -0400

    Add search bar to filter list by dj

--- app/src/main/java/com/example/radioviewer/view/MainActivity.java
+++ app/src/main/java/com/example/radioviewer/view/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.radioviewer.view;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
@@ -7,6 +11,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.Toast;
@@ -17,11 +25,15 @@ import com.example.radioviewer.model.MusicList;
 import com.example.radioviewer.presenter.MainPresenter;
 import com.example.radioviewer.presenter.MainPresenterContract;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements MainViewContract {
 
     private RecyclerView recyclerView;
     private MainPresenterContract presenter;
     private boolean fragmentPresent = false;
+    private String query = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,12 +53,49 @@ public class MainActivity extends AppCompatActivity implements MainViewContract 
                 fragmentPresent = false;
         });
 
+        Intent intent = getIntent();
+        handleIntent(intent);
+    }
 
-        //ActionBar actionBar = getSupportActionBar();
+    @Override protected void onNewIntent(Intent intent) {
+        setIntent(intent);
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            query = intent.getStringExtra(SearchManager.QUERY).toUpperCase();
+            presenter.requestMusicList();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.search:
+                onSearchRequested();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
     public void displayMusicList(MusicList musicList) {
+        if (query != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                musicList.getChannels().removeIf(channel ->
+                        !channel.getDj().toUpperCase().contains(query));
+            } else {
+                List<Channel> filteredList = new ArrayList<>();
+                for (Channel chan : musicList.getChannels()) {
+                    if (chan.getDj().toUpperCase().contains(query)) {
+                        filteredList.add(chan);
+                    }
+                }
+                musicList.setChannels(filteredList);
+            }
+        }
         recyclerView.setAdapter(new RadioRecyclerAdapter(musicList, this));
     }
 
@@ -70,5 +119,17 @@ public class MainActivity extends AppCompatActivity implements MainViewContract 
                 .commit();
 
         fragmentPresent = true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false);
+
+        return true;
     }
 }
